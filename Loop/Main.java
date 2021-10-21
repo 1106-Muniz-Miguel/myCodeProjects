5dc1d494d5e3c6b8ba99cc5017b5c1f013072b9c
Rogue-King <miguel@muniz.org>
2021-10-21 13:33:59 -0700

    Initial commit of Loop/

---
+++ Loop/Main.java
@@ -0,0 +1,10 @@
+package Loop;
+
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            String[] animals = {"Bird","Cat","Dog","Pig","Sheep"};
+            System.out.println(animals[i]);        
+        }  
+    }
+}
